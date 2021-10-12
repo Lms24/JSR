566f25af4003f28fccaed19948f423dadb3312ee
Lukas Stracke <l.stracke@student.tugraz.at>
2021-10-12 16:45:17 +0200

    Make params and settings panel collapsible

--- jsr/JSR-IntelliJ-Plugin/src/main/java/at/tugraz/ist/stracke/jsr/intellij/JSRToolWindow.java
+++ jsr/JSR-IntelliJ-Plugin/src/main/java/at/tugraz/ist/stracke/jsr/intellij/JSRToolWindow.java
@@ -2,13 +2,24 @@
 
 package at.tugraz.ist.stracke.jsr.intellij;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.TextBrowseFolderListener;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetWrapper;
+import com.intellij.util.PlatformIcons;
+import org.apache.xmlbeans.impl.xb.xsdschema.All;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.plaf.IconUIResource;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class JSRToolWindow {
 
@@ -35,20 +46,51 @@ public class JSRToolWindow {
   private JLabel lblSettingsWrapper;
   private JPanel pnlParamsWrapper;
   private JLabel lblParamsWrapper;
-  private JComboBox cbCovMetric;
+  private ComboBox<String> cbCovMetric;
   private JLabel lblCovMetric;
   private JLabel lblReductionAlg;
-  private JComboBox cbReductionAlg;
+  private ComboBox<String> cbReductionAlg;
   private JCheckBox chkLastReport;
+  private JPanel pnlParams;
 
   public JSRToolWindow(ToolWindow toolWindow) {
+    initIcons();
+    listenToPanelClicks(lblSettingsWrapper, pnlSettings);
+    listenToPanelClicks(lblParamsWrapper, pnlParams);
   }
 
   public JPanel getContent() {
     return jsrToolWindowContent;
   }
 
+  private void initIcons() {
+    lblSettingsWrapper.setIcon(AllIcons.Ide.Notification.Expand);
+    lblParamsWrapper.setIcon(AllIcons.Ide.Notification.Expand);
+  }
+
+  private void listenToPanelClicks(JLabel wrapperLabel, JPanel panel) {
+    wrapperLabel.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseClicked(MouseEvent e) {
+        boolean nowVisible = !panel.isVisible();
+        panel.setVisible(nowVisible);
+        final Icon newIcon = nowVisible ? AllIcons.Ide.Notification.Expand : AllIcons.Actions.ArrowExpand;
+        wrapperLabel.setIcon(newIcon);
+        super.mouseClicked(e);
+      }
+    });
+  }
+
+
+  /**
+   * Entry point for component creation
+   */
   private void createUIComponents() {
+    initFileBrowsers();
+    initComboBoxes();
+  }
+
+  private void initFileBrowsers() {
     tfTestPath = new TextFieldWithBrowseButton();
     tfSourcePath = new TextFieldWithBrowseButton();
     tfJarPath = new TextFieldWithBrowseButton();
@@ -78,5 +120,22 @@ public class JSRToolWindow {
     tfSlicerPath.addBrowseFolderListener(new TextBrowseFolderListener(chooseFolderDescriptor));
     tfOutputPath.addBrowseFolderListener(new TextBrowseFolderListener(chooseFolderDescriptor));
     tfSerialPath.addBrowseFolderListener(new TextBrowseFolderListener(chooseFolderDescriptor));
+  }
+
+  private void initComboBoxes() {
+    ResourceBundle rb = ResourceBundle.getBundle("i18n/en");
+    List<String> covMetricItems = Arrays.asList(
+      rb.getString("cov.checked"),
+      rb.getString("cov.line"),
+      rb.getString("cov.method")
+    );
+
+    List<String> algorithmItems = Arrays.asList(
+      rb.getString("alg.hgs"),
+      rb.getString("alg.gen")
+    );
+
+    cbCovMetric = new ComboBox<>(covMetricItems.toArray(new String[0]));
+    cbReductionAlg = new ComboBox<>(algorithmItems.toArray(new String[0]));
   }
 }
